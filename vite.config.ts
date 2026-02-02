c149c35c3daa17f06c7138e9dd673be92ff7a8b1
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-02-02 20:02:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-const config = defineConfig({
+export default defineConfig({
+  ssr: {
+    noExternal: ["@tabler/icons-react"],
+  },
+
   plugins: [
     devtools(),
+
     nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+
+    tsconfigPaths({
+      projects: ["./tsconfig.json"],
     }),
+
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
+    react(),
   ],
-})
-
-export default config
+});
