4851a78549f0403ecb9e4a6cb12da9af707883ba
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-02-02 20:18:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,23 @@
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   ssr: {
     noExternal: ["@tabler/icons-react"],
   },
 
   plugins: [
     devtools(),
-
-    nitro(),
-
-    tsconfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
     react(),
-  ],
-});
+
+    command === "build" && nitro(),
+  ].filter(Boolean),
+}));
