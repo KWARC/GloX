12721e13b370ab89de65784f4b362e370ecf7832
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-02-09 18:30:56 +0530

    minor changes

--- src/server/ftml/generateStexFromFtml.ts
+++ src/server/ftml/generateStexFromFtml.ts
@@ -11,24 +11,21 @@ export async function generateStexFromFtml(
   const fd = floDown.FloDown.fromUri("http://temp?a=temp&d=temp&l=en");
 
   const root = normalizeToRoot(ftmlAst);
-  const block = root.content[0];
-  if (!block) return "";
+  for (const block of root.content) {
+    if (block.type === "paragraph") {
+      fd.addElement(block);
+      continue;
+    }
 
-  if (block.type === "paragraph") {
-    fd.addElement(block);
-    return fd.getStex();
+    if (block.type !== "definition") continue;
+
+    const symbolName = block.for_symbols?.[0];
+    if (!symbolName) continue;
+
+    const symbol = fd.addSymbolDeclaration(symbolName);
+    const finalizedFTML = finalFloDown(block, symbol);
+    fd.addElement(finalizedFTML);
   }
-
-  if (block.type !== "definition") return "";
-
-  const symbolName = block.for_symbols?.[0];
-  if (!symbolName) return "";
-
-  const symbol = fd.addSymbolDeclaration(symbolName);
-
-  const finalizedFTML = finalFloDown(block, symbol);
-
-  fd.addElement(finalizedFTML);
 
   return fd.getStex();
 }
