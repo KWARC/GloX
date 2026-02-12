72900d3749b6e75d8adb9511b64120c866133577
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-02-12 22:54:08 +0530

    get finalized latex

--- src/server/ftml/generateStexFromFtml.ts
+++ src/server/ftml/generateStexFromFtml.ts
@@ -1,29 +1,69 @@
 import { initFloDown } from "@/lib/flodownClient";
 import {
-  FtmlStatement,
-  normalizeToRoot,
+  DefiniendumNode,
   DefinitionNode,
   FtmlContent,
   FtmlNode,
+  FtmlStatement,
+  normalizeToRoot,
 } from "@/types/ftml.types";
 
-function finalFTML(
-  node: FtmlNode,
-  uriMap: Map<string, string>,
-): FtmlNode {
+function isMathHubUri(uri: string): boolean {
+  return (
+    uri.startsWith("http://mathhub.info?") ||
+    uri.startsWith("https://mathhub.info?")
+  );
+}
+
+function finalFTML(node: FtmlNode, uriMap: Map<string, string>): FtmlNode {
   if (node.type === "definition") {
     const def = node as DefinitionNode;
 
     return {
       ...def,
-      for_symbols: def.for_symbols.map(
-        (s) => uriMap.get(s) ?? s,
-      ),
+      for_symbols: def.for_symbols.map((s) => uriMap.get(s) ?? s),
       content: rewriteContent(def.content, uriMap),
     };
   }
 
-  if (node.type === "definiendum" || node.type === "symref") {
+  if (node.type === "definiendum") {
+    const defNode = node as DefiniendumNode;
+
+    if (
+      defNode.symdecl === false &&
+      defNode.uri &&
+      !defNode.uri.startsWith("http")
+    ) {
+      const symbolName = defNode.uri;
+      const tempUri = `http://temp?a=temp&m=temp&s=${symbolName}`;
+
+      return {
+        ...defNode,
+        uri: tempUri,
+        content: rewriteContent(defNode.content ?? [], uriMap),
+      };
+    }
+
+    return {
+      ...defNode,
+      uri: uriMap.get(defNode.uri!) ?? defNode.uri,
+      content: rewriteContent(defNode.content ?? [], uriMap),
+    };
+  }
+
+  if (node.type === "symref") {
+    const symrefUri = node.uri;
+
+    if (symrefUri && !isMathHubUri(symrefUri)) {
+      const symbolName = symrefUri;
+      const tempUri = `http://temp?a=temp&m=temp&s=${symbolName}`;
+      return {
+        ...node,
+        uri: tempUri,
+        content: rewriteContent(node.content ?? [], uriMap),
+      };
+    }
+
     return {
       ...node,
       uri: uriMap.get(node.uri!) ?? node.uri,
@@ -51,15 +91,46 @@ function rewriteContent(
   });
 }
 
+function collectSymbolsToRemove(content: FtmlContent[]): Set<string> {
+  const toRemove = new Set<string>();
+
+  for (const item of content) {
+    if (typeof item === "string") continue;
+
+    if (item.type === "definiendum") {
+      const defNode = item as DefiniendumNode;
+      if (
+        defNode.symdecl === false &&
+        defNode.uri &&
+        !defNode.uri.startsWith("http")
+      ) {
+        toRemove.add(defNode.uri);
+      }
+    }
+
+    if (item.type === "symref") {
+      const symrefUri = item.uri;
+      if (symrefUri && !isMathHubUri(symrefUri)) {
+        toRemove.add(symrefUri);
+      }
+    }
+
+    if (item.content) {
+      const nested = collectSymbolsToRemove(item.content);
+      nested.forEach((s) => toRemove.add(s));
+    }
+  }
+
+  return toRemove;
+}
+
 export async function generateStexFromFtml(
   ftmlAst: FtmlStatement,
+  fileName: string,
 ): Promise<string> {
   const floDown = await initFloDown();
   floDown.setBackendUrl("https://mmt.beta.vollki.kwarc.info");
-
-  const fd = floDown.FloDown.fromUri(
-    "http://temp?a=temp&d=temp&l=en",
-  );
+  const fd = floDown.FloDown.fromUri(`http://temp?a=temp&d=${fileName}&l=en`);
 
   const root = normalizeToRoot(ftmlAst);
 
@@ -72,18 +143,28 @@ export async function generateStexFromFtml(
     if (block.type !== "definition") continue;
 
     const def = block as DefinitionNode;
+    const symbolsToRemove = collectSymbolsToRemove(def.content);
+
+    const filteredForSymbols = def.for_symbols.filter(
+      (symbolName) => !symbolsToRemove.has(symbolName),
+    );
+
     const uriMap = new Map<string, string>();
 
-    for (const symbolName of def.for_symbols) {
+    for (const symbolName of filteredForSymbols) {
       if (!symbolName.startsWith("http")) {
-        const declaredUri =
-          fd.addSymbolDeclaration(symbolName);
+        const declaredUri = fd.addSymbolDeclaration(symbolName);
         uriMap.set(symbolName, declaredUri);
       }
     }
 
-    const ftmlEle = finalFTML(block, uriMap);
-    fd.addElement(ftmlEle);
+    const transformedDef = {
+      ...def,
+      for_symbols: filteredForSymbols.map((s) => uriMap.get(s) ?? s),
+      content: rewriteContent(def.content, uriMap),
+    };
+
+    fd.addElement(transformedDef);
   }
 
   return fd.getStex();
