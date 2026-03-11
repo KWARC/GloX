507e9b5256b713154f2b20e272bf97f11f8ccf43
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-03-11 09:29:04 +0530

    new page for curation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { CurationSection } from "@/components/CurationSection";
 import { FinalizedDocumentsSection } from "@/components/FinalizedDocumentsSection";
 import UploadDialog from "@/components/UploadDialog";
 import { currentUser } from "@/server/auth/currentUser";
-import { Button, Divider, Stack, Text, Title } from "@mantine/core";
+import { Button, Stack, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
@@ -13,7 +12,6 @@ export const Route = createFileRoute("/")({
 
 function App() {
   const [opened, setOpened] = useState(false);
-  const [curationLevel, setCurationLevel] = useState<string | null>(null);
 
   const { data: user } = useQuery({
     queryKey: ["currentUser"],
@@ -38,16 +36,7 @@ function App() {
           </Button>
         </Stack>
 
-        {isLoggedIn && (
-          <>
-            <CurationSection
-              curationLevel={curationLevel}
-              setCurationLevel={setCurationLevel}
-            />
-            <Divider w="100%" maw={1000} />
-            <FinalizedDocumentsSection />
-          </>
-        )}
+        {isLoggedIn && <FinalizedDocumentsSection />}
       </Stack>
 
       <UploadDialog opened={opened} onClose={() => setOpened(false)} />
